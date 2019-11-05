4ffef8fe2fc5ef0d57a9762191988471c8440e30
Alexander Gusev <san4es021@gmail.com>
2019-11-05 18:58:51 +0300

    ok

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,21 @@
 //= libs/jquery.min.js
+//= libs/wow.min.js
 
-$('#showMenu').click(function() {
-  $('.l-header .header__menu.mobile').addClass('active')
-  event.stopPropagation()
+$(document).ready(function() {
+  new WOW({
+    mobile: false
+  }).init()
 
-  $(document).click(function() {
-    $('.l-header .header__menu.mobile').removeClass('active')
-  })
-
-  $('.l-header .header__menu.mobile .menu-wrapper').click(function(event) {
+  $('#showMenu').click(function() {
+    $('.l-header .header__menu.mobile').addClass('active')
     event.stopPropagation()
+
+    $(document).click(function() {
+      $('.l-header .header__menu.mobile').removeClass('active')
+    })
+
+    $('.l-header .header__menu.mobile .menu-wrapper').click(function(event) {
+      event.stopPropagation()
+    })
   })
 })
